e3ff7ebe4ef06b3895f8861e2801d3102ee602ba
Parfax <parfaxs@gmail.com>
2022-09-11 15:41:59 +0300

    clean LessMess but with some troubles

---
+++ Less Mess.py
@@ -0,0 +1,32 @@
+import os
+import json
+
+
+configuration = open('./parameters.json')
+data = json.load(configuration)
+
+folders = []
+for dirPath in data['path']:
+    path = os.path.expanduser(data['path'][dirPath])
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+    folders.append(path)
+
+track = folders[0]
+
+
+def handle():
+    for filename in os.listdir(track):
+        extension = filename.split('.')
+        if len(extension) > 1:
+            file = track + '/' + filename
+
+            for tuple in enumerate(data['extension']):
+                if extension[-1].lower() in data['extension'][tuple[1]]:
+                    new_path = folders[tuple[0] + 1] + '/' + filename
+                    os.rename(file, new_path)
+
+
+handle()
+configuration.close()
