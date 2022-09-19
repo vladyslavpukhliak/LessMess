3bcd274decf98246415247401bd70dadaf9a1ba8
Parfax <parfaxs@gmail.com>
2022-09-19 13:09:33 +0300

    lil update

--- Less Mess.py
+++ Less Mess.py
@@ -2,7 +2,6 @@ import os
 import sys
 import json
 
-
 cwd = os.path.dirname(sys.executable)
 configuration = os.path.join(cwd, 'parameters.json')
 data = json.load(open(configuration))
@@ -24,11 +23,10 @@ def handle():
         if len(extension) > 1:
             file = track + '/' + filename
 
-            for tuple in enumerate(data['extension']):
-                if extension[-1].lower() in data['extension'][tuple[1]]:
-                    new_path = folders[tuple[0] + 1] + '/' + filename
+            for index, name in enumerate(data['extension']):
+                if extension[-1].lower() in data['extension'][name]:
+                    new_path = folders[index + 1] + '/' + filename
                     os.rename(file, new_path)
 
 
 handle()
-# configuration.close()
